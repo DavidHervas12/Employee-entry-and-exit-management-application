5b61d9f8e0bd96253cd66732cf2656d12c3a0d08
DavidHervas12 <121380511+DavidHervas12@users.noreply.github.com>
2023-04-21 14:38:42 +0200

    Creacion de la conexion a base de datos

---
+++ Clases/ConexionBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+
+namespace AEV7_David_Alberto.Clases
+{
+    internal class ConexionBD
+    {
+
+    }
+}
