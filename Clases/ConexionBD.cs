e09a4c90353e15907b9bb2831a9d9f43c6407df7
DavidHervas12 <121380511+DavidHervas12@users.noreply.github.com>
2023-04-21 15:19:03 +0200

    Clase Base de datos

--- Clases/ConexionBD.cs
+++ Clases/ConexionBD.cs
@@ -4,11 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace AEV7_David_Alberto.Clases
 {
     internal class ConexionBD
     {
+        private static MySqlConnection instancia = null;
 
+        private static readonly object padlock = new object();
+
+        private ConexionBD () { }
+
+        public static MySqlConnection Conexion
+        {
+            get
+            {
+                lock(padlock)
+                {
+                    if(instancia == null)
+                    {
+                        instancia = new MySqlConnection();
+                        instancia.ConnectionString = "sever=127.0.0.1;database=BDFichajes;uid=root;pwd=";
+                    }
+                    return instancia;
+                }
+            }
+        }
+        public static void AbrirConexion()
+        {
+            if (instancia!=null)
+            {
+                instancia.Open();
+            }
+        }
+
+        public static void CerrarConexion()
+        {
+            if (instancia!=null)
+            {
+                instancia.Close();
+            }
+        }
     }
 }
