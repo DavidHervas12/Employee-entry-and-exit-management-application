63ca6b2b394503b3cd7211a35c1236586a8ca8f6
Alberto Villegas <alvich@floridauniversitaria.es>
2023-04-28 17:26:18 +0200

    Validando formularios

--- FrmClave.cs
+++ FrmClave.cs
@@ -31,14 +31,18 @@ namespace AEV7_David_Alberto
                     MessageBox.Show("Accediendo a las funcionalidades de mantenimiento");
                     this.Dispose();
                     ConexionBD.CerrarConexion(); //Cerramos conexion de este formulario para no tener conflictos con el otro
-                    frmMantenimiento = new FrmMantenimiento();
-                    frmMantenimiento.Show();
 
-                    if (frmMantenimiento != null)
+                    if (frmMantenimiento == null) //Si el formulario es nulo y no se ha instanciado el objeto se hará uno nuevo, sino se activará el mismo
+                    {
+                        frmMantenimiento = new FrmMantenimiento();
+                        frmMantenimiento.Show();
+                    }
+                    else
                     {
                         frmMantenimiento.Activate();
                     }
-                } else
+                } 
+                else
                 {
                     MessageBox.Show("La clave introducida del administrador no es correcta", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
